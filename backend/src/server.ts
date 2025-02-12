67224629670bd4281f7f48090b84ab265a5cab57
Mandeep Singh <mandeeparora132@gmail.com>
2025-02-13 00:24:02 +0530

    add config for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
+import cors from "cors";
 import authRoutes from "./routes/auth";
 import { errorHandler } from "./middleware/error";
+import { keepAlive } from "./utils/keepAlive";
 const app = express();
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  FRONTEND_URL
+]
+
+// CORS
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 // Middleware
 app.use(express.json());
@@ -23,6 +37,17 @@ app.use('/api/v1/auth', authRoutes);
 
 // Error Handler
 app.use(errorHandler);
+
+// Keep-alive cron job (to avoid cold start on render.com)
+(async () => {
+  keepAlive(process.env.BACKEND_URL).then((job) => {
+    if (job) {
+      job.start();
+    }
+  }).catch((error) => {
+    console.error("Error starting keep-alive job:", error);
+  });
+})();
 
 app.listen(port, () => {
   return console.log(`Server is listening at http://localhost:${port}`);
---
+++ backend/src/utils/keepAlive.ts
@@ -0,0 +1,38 @@
+import { CronJob } from "cron";
+import https from "https";
+
+export async function keepAlive(backendURL: string) {
+  if (!backendURL) {
+    console.error("No backend URL provided");
+    return null;
+  }
+  try {
+    const job = new CronJob("*/13 * * * *", async () => {
+      // Send a GET request to the server to keep it alive
+      console.log("Sending keep-alive request...");
+      https
+        .get(backendURL, (res) => {
+          if (res.statusCode === 200) {
+            res.setEncoding("utf8");
+            res.on("data", function (data) {
+              console.log("Keep-alive response:", data);
+            });
+            console.log("Keep-alive request sent successfully.");
+          } else {
+            console.log(
+              "Keep-alive request failed with status code:",
+              res.statusCode
+            );
+          }
+        })
+        .on("error", (error) => {
+          console.error("Error sending keep-alive request:", error);
+        });
+    });
+
+    return job;
+  } catch (error) {
+    console.error("Error sending keep-alive request:", error);
+    return null;
+  }
+}
